ce177dae90f4e93a08fd1c0dda67c430b8d9a38d
Rian McGuire <rian@rian.id.au>
2023-12-05 16:56:25 +1100

    Refactor

--- 05b.rb
+++ 05b.rb
@@ -7,20 +7,12 @@ seeds, *maps = ARGF.read.split("\n\n")
 seeds = seeds.split(": ").last.split(" ").map(&:to_i).each_slice(2).to_a
 
 RangeMapping = Struct.new(:dest_start, :source_start, :length) do
-    def dest_range
-        (dest_start..(dest_start+length-1))
-    end
-
     def source_range
         (source_start..(source_start+length-1))
     end
 
     def map(source)
         dest_start + (source - source_start)
-    end
-
-    def map_range(range)
-
     end
 end
 
@@ -30,50 +22,20 @@ maps.map! do |block|
     end
 end
 
-class Range
-    def overlap?(other)
-        other.begin <= self.end && self.begin <= other.end 
-    end
-
-    def intersection(other)
-      return nil if (self.max < other.begin or other.max < self.begin) 
-      [self.begin, other.begin].max..[self.max, other.max].min
-    end
-end
-
-def merge_ranges(ranges)
-    merged = []
-    ranges.sort_by {|r| r.begin}.each do |r|
-      raise ArgumentError if r.exclude_end?
-
-      if merged.empty? || merged[-1].end < r.begin
-        merged << r
-      else
-        merged[-1] = Range.new(merged[-1].begin, [merged[-1].end, r.end].max)
-      end
-    end
-    merged
-end
-
 seeds.map do |start, length|
     value_ranges = MultiRange.new([(start..(start+length-1))])
 
-    maps.each_with_index do |mappings, index|
-        puts "mapping #{index}"
-        pp "input", value_ranges
-
+    maps.each do |mappings|
+        source_ranges = MultiRange.new([])
         destination_ranges = MultiRange.new([])
 
-        source_ranges = MultiRange.new([])
         mappings.each do |mapping|
             source_ranges |= mapping.source_range
 
-            value_ranges.ranges.each do |range|
-                source_intersection = mapping.source_range.intersection(range)
-                if source_intersection
-                    destination_range = (mapping.map(source_intersection.begin)..mapping.map(source_intersection.end))
-                    destination_ranges |= destination_range
-                end
+            source_intersection = MultiRange.new([mapping.source_range]) & value_ranges
+            source_intersection.ranges.each do |source_range|
+                destination_range = (mapping.map(source_intersection.min)..mapping.map(source_intersection.max))
+                destination_ranges |= destination_range
             end
         end
 
