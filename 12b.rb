04e87b1e2f8a54d82cfd857f667f63bec5066d26
Rian McGuire <rian@rian.id.au>
2023-12-12 21:40:51 +1100

    Day 12 part 2 go faster

--- 12b.rb
+++ 12b.rb
@@ -26,10 +26,9 @@ def search(springs, counts, matched = [], memo = {})
     end
 
     if (springs.count { _1 == "#"} == 0) && counts.empty?
-        # puts "#{matched.join("")}"
-        if !matches_counts(matched, $original_counts)
-            raise "WTF: arrangement doesn't match original counts"
-        end
+        # if !matches_counts(matched, $original_counts)
+        #     raise "WTF: arrangement doesn't match original counts"
+        # end
         return 1
     end
 
@@ -40,16 +39,17 @@ def search(springs, counts, matched = [], memo = {})
     space_required = counts.sum + (counts.length - 1)
 
     if space_required > springs.length
-        # puts "not enough space"
         return 0
     end
 
-    # puts "original: #{springs.join("")}"
-    # puts "counts: #{counts.join(",")}"
+    # First group doesn't have a confirmed length yet, but it's bigger than the expected count
+    first_group = springs.join.match(/^\.*(#+)\?/)
+    if first_group && first_group[1].length > counts[0]
+        return 0
+    end
 
     # Try to match the head of springs with the head of counts
     matched_counts = springs.slice_when { |a, b| a != b && b != "?" }.take_while { |g| !g.include?("?") }.filter { |g| g.all? { _1 == "#" } }.map(&:length)
-    # puts "matched_counts: #{matched_counts.join(",")}"
 
     if matched_counts.any? 
         if matched_counts == counts[0...matched_counts.length]
@@ -60,16 +60,15 @@ def search(springs, counts, matched = [], memo = {})
                 result
             end
 
-            if springs[...matched_chunk.length] != matched_chunk
-                raise "wtf"
-            end
+            # if springs[...matched_chunk.length] != matched_chunk
+            #     raise "wtf"
+            # end
 
             remaining_springs = springs[matched_chunk.length..]
             remaining_counts = counts[matched_counts.length..]
 
-            return search(remaining_springs, remaining_counts, matched + matched_chunk, memo)
+            return search(remaining_springs, remaining_counts, matched, memo)
         else
-            # puts "going up"
             return 0
         end
     end
@@ -83,6 +82,7 @@ def search(springs, counts, matched = [], memo = {})
 
     count = 0
 
+    # TODO: stop using permutations and "slide" the first group length across the unknowns
     permutation_length = [counts.first, unknown_indexes.length].min
     [".", "#"].repeated_permutation(permutation_length).each do |replacements|
         modified = springs.dup
@@ -92,35 +92,6 @@ def search(springs, counts, matched = [], memo = {})
 
         count += search(modified, counts, matched, memo)
     end
-
-    # spring_chunk = springs[..chunk_index]
-    # puts "chunk: #{spring_chunk.join("")}"
-    # unknown_indexes = spring_chunk.each_with_index.filter { |c, i| c == "?" }.map(&:last)
-
-    # count = 0
-    # [".", "#"].repeated_permutation(unknown_indexes.length).each do |replacements|
-    #     modified = spring_chunk.dup
-    #     replacements.reverse.zip(unknown_indexes).each do |r, i|
-    #         modified[i] = r
-    #     end
-
-    #     # puts "modified: #{modified.join("")}"
-
-    #     if modified_counts == counts[0...modified_counts.length]
-
-
-    #         # Matches prefix of counts, go deeper
-    #         remaining_springs = springs[spring_chunk.length..]
-    #         remaining_counts = counts[modified_counts.length..]
-
-    #         if modified[-1] == "#" && remaining_springs[0] == "?"
-    #             # raise "uh oh"
-    #             remaining_springs[0] = "."
-    #         end
-
-    #         count += search(remaining_springs, remaining_counts, matched + modified, memo)
-    #     end
-    # end
 
     memo[[springs, counts]] = count
     count
