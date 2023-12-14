e9780f548781da8489fc38d018da21c212a0c302
Rian McGuire <rian@rian.id.au>
2023-12-14 20:51:08 +1100

    Day 14 part 2 optimisation

--- 14b.rb
+++ 14b.rb
@@ -1,110 +1,103 @@
 #!/usr/bin/env ruby
 
-Pos = Struct.new(:x, :y) do
-    def step(dir)
-        case dir
-        when :n
-            Pos.new(x, y - 1)
-        when :s
-            Pos.new(x, y + 1)
-        when :e
-            Pos.new(x + 1, y)
-        when :w
-            Pos.new(x - 1, y)
-        end
-    end
-end
-
-rocks = Set.new
 rocks_by = {
-    x: Hash.new { |h, k| h[k] = Set.new },
-    y: Hash.new { |h, k| h[k] = Set.new },
+    x: Hash.new { |h, k| h[k] = [] },
+    y: Hash.new { |h, k| h[k] = [] },
 }
-blocks = Set.new
+blocks_by = {
+    x: Hash.new { |h, k| h[k] = [] },
+    y: Hash.new { |h, k| h[k] = [] },
+}
 
-ARGF.each_line.each_with_index do |line, y|
-    line.chomp.chars.each_with_index do |c, x|
+lines = ARGF.each_line.map(&:chomp)
+lines.each_with_index do |line, y|
+    line.chars.each_with_index do |c, x|
         if c == "O"
-            pos = Pos.new(x, y)
-            rocks << pos
-            rocks_by[:x][pos.x] << pos
-            rocks_by[:y][pos.y] << pos
+            rocks_by[:x][x] << y
+            rocks_by[:y][y] << x
         elsif c == "#"
-            blocks << Pos.new(x, y)
+            blocks_by[:x][x] << y
+            blocks_by[:y][y] << x
         end
     end
 end
 
-$max_x = max_x = (rocks + blocks).map(&:x).max
-$max_y = max_y = (rocks + blocks).map(&:y).max
+$max_y = max_y = lines.length - 1
+$max_x = max_x = lines.first.length - 1
 
 $known_states = {}
+
+# Compact a 1D stack of rock indexes towards the left or right
+def compact(rocks, blockers, right)
+    result = []
+
+    # TODO: pre-sort on load
+    blockers.sort!
+
+    [-1, *blockers, $max_y + 1].each_cons(2).map { |a, b| Range.new(a + 1, b - 1, false) }.each do |range|
+        # TODO: take advantage of rocks being sorted
+        count = rocks.count { range.include?(_1) }
+        count.times do |x|
+            if right
+                result << range.end - x
+            else
+                result << range.begin + x
+            end
+        end
+    end
+
+    result
+end
 
 cycles = 1000000000
 cycle = 0
 loop do
     cycle += 1
-    puts "cycle #{cycle}"
     [:n, :w, :s, :e].each do |dir|
-        loop do
-            any_moved = false
+        range, axis, opposite_axis, reverse = case dir
+        when :n 
+            [(0..max_x), :x, :y, false]
+        when :s
+            [(0..max_x), :x, :y, true]
+        when :e
+            [(0..max_y), :y, :x, true]
+        when :w
+            [(0..max_y), :y, :x, false]
+        end
 
-            range, axis = case dir
-            when :n 
-                [(1..max_y), :y]
-            when :s
-                [(0..max_y - 1).reverse_each, :y]
-            when :e
-                [(0..max_x - 1).reverse_each, :x]
-            when :w
-                [(1..max_x), :x]
-            end
-        
-            range.each do |n|
-                rocks_by[axis][n].each do |rock|
-                    new_rock = rock.step(dir)
-        
-                    if !rocks.include?(new_rock) && !blocks.include?(new_rock)
-                        rocks.delete rock
-                        rocks_by[:x][rock.x].delete rock
-                        rocks_by[:y][rock.y].delete rock
-
-                        rocks << new_rock
-                        rocks_by[:x][new_rock.x] << new_rock
-                        rocks_by[:y][new_rock.y] << new_rock
-
-                        any_moved = true
-                    end
-                end
+        range.each do |n|
+            rocks = rocks_by[axis][n]
+            rocks.each do |j|
+                rocks_by[opposite_axis][j].delete n
             end
 
-            break unless any_moved
+            new_rocks = compact(rocks, blocks_by[axis][n], reverse)
+
+            rocks_by[axis][n] = new_rocks
+            new_rocks.each do |j|
+                # TODO: insert sorted
+                rocks_by[opposite_axis][j] << n
+            end
         end
     end
 
-    if prev = $known_states[rocks.dup]
-        puts "at cycle #{cycle}"
-        puts "previously seen at #{prev}"
+    state = rocks_by[:x].values.map { _1.dup }
 
+    if prev = $known_states[state]
         repeat_length = cycle - prev
-        puts "repeat_length #{repeat_length}"
-
         remaining = cycles - cycle
-        puts "remaining #{remaining}"
-
-        skip_cycles = remaining / repeat_length
-
-        cycle += skip_cycles * repeat_length
-
-        puts "new cycle #{cycle}"
+        skip_repeats = remaining / repeat_length
+        cycle += skip_repeats * repeat_length
         $known_states = {}
     else
-        $known_states[rocks.dup] = cycle
+        $known_states[state] = cycle
     end
 
     break if cycle == cycles
 end
 
-rocks.sum do |rock|
-    max_y + 1 - rock.y
+rocks_by[:x].values.sum do |ys|
+    ys.sum do |y|
+        max_y + 1 - y
+    end
 end.then { puts _1 }
