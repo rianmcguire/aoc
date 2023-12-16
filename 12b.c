ba57895bb1f2f44c01864d99f2df586f41ad0dbb
Rian McGuire <rian@rian.id.au>
2023-12-16 12:45:50 +1100

    Tweak allocation sizes

--- 12b.c
+++ 12b.c
@@ -4,14 +4,18 @@
 #include <stdbool.h>
 #include <search.h>
 
+#define SPRINGS_MAX 128
+#define COUNTS_MAX 64
+#define HASH_TABLE_SIZE 1000
+
 int64_t search(char* springs, int* counts, int counts_len) {
     // Skip over leading "." - they don't affect the result
     while (springs[0] == '.') springs++;
 
     // Check memoization hash table
     ENTRY item;
-    item.key = malloc(1000);
-    snprintf(item.key, 1000, "%s%d", springs, counts_len);
+    item.key = malloc(SPRINGS_MAX);
+    snprintf(item.key, SPRINGS_MAX, "%s%d", springs, counts_len);
     ENTRY *found;
     if ((found = hsearch(item, FIND)) != NULL) {
         return (int64_t)found->data;
@@ -43,18 +47,13 @@ int64_t search(char* springs, int* counts, int counts_len) {
         }
     } else {
         char* unknown_p = strchr(springs, '?');
-        if (unknown_p[0] == '\0') {
-            printf("wtf\n");
-            exit(1);
-        }
-
         int unknown_idx = unknown_p - springs;
 
-        char with_working[1000];
+        char with_working[SPRINGS_MAX];
         strlcpy(with_working, springs, sizeof(with_working));
         with_working[unknown_idx] = '.';
 
-        char with_broken[1000];
+        char with_broken[SPRINGS_MAX];
         strlcpy(with_broken, springs, sizeof(with_broken));
         with_broken[unknown_idx] = '#';
 
@@ -69,20 +68,20 @@ int64_t search(char* springs, int* counts, int counts_len) {
 
 int main() {
     int64_t sum = 0;
-    char buffer[1000];
+    char buffer[SPRINGS_MAX];
     while (fgets(buffer, sizeof(buffer), stdin) != NULL) {
         char *buffer_p = buffer;
         char *springs = strsep(&buffer_p, " ");
         char *counts_str = buffer_p;
 
         // Unfold
-        char springs_unfolded[1000];
+        char springs_unfolded[SPRINGS_MAX];
         snprintf(springs_unfolded, sizeof(springs_unfolded), "%s?%s?%s?%s?%s", springs, springs, springs, springs, springs);
-        char counts_unfolded[1000];
+        char counts_unfolded[COUNTS_MAX];
         snprintf(counts_unfolded, sizeof(counts_unfolded), "%s,%s,%s,%s,%s", counts_str, counts_str, counts_str, counts_str, counts_str);
 
         // Parse counts
-        int counts[1000];
+        int counts[COUNTS_MAX];
         int counts_len = 0;
         char* count;
         char* count_p = counts_unfolded;
@@ -90,7 +89,7 @@ int main() {
             counts[counts_len++] = atoi(count);
         }
 
-        hcreate(1000);
+        hcreate(HASH_TABLE_SIZE);
         int64_t result = search(springs_unfolded, counts, counts_len);
         hdestroy();
 
