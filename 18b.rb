15108aef30b361746ebec64cbf2b797b8c5d9163
Rian McGuire <rian@rian.id.au>
2023-12-19 08:03:03 +1100

    Day 18

--- 18b.rb
+++ 18b.rb
@@ -17,19 +17,6 @@ end
 
 lines = ARGF.each_line.to_a
 
-# Brute force
-pos = Pos.new(0, 0)
-holes = Set.new([pos])
-lines.each do |line|
-    dir, dist, color = line.chomp.split(" ")
-    dist = dist.to_i
-
-    dist.times do
-        pos = pos.step(dir, 1)
-        holes << pos
-    end
-end
-
 def rotation(last_dir, dir)
     if last_dir == "U" && dir == "R"
         90
@@ -58,8 +45,13 @@ last_dir = nil
 poly = [pos]
 lines.each do |line|
     puts line
-    dir, dist, color = line.chomp.split(" ")
-    dist = dist.to_i
+    _, _, color = line.chomp.split(" ")
+
+    color = color[2..]
+
+    dist_hex = color[...5]
+    dist = dist_hex.to_i(16)
+    dir = ["R", "D", "L", "U"][color[5].to_i]
 
     rot = rotation(last_dir, dir)
     puts "rot #{rot}"
@@ -93,40 +85,4 @@ def area(poly)
     area.abs / 2.0
 end
 
-def flood(holes, pos)
-    stack = [pos]
-
-    until stack.empty?
-        pos = stack.pop
-
-        next if holes.include?(pos)
-
-        holes << pos
-
-        ["U", "D", "L", "R"].each do |dir|
-            new_pos = pos.step(dir, 1)
-            stack << new_pos
-        end
-    end
-end
-
-# pp poly
-
-puts "Outline: #{holes.length}"
-
-# TODO: lol
-flood(holes, Pos.new(0, 0).step("D", 1).step("R", 1))
-
-puts "Flood area: #{holes.length}"
-
-
 puts "Shoelace area: #{area(poly)}"
-
-# # https://en.wikipedia.org/wiki/Pick's_theorem
-# interior = area(poly)
-
-# pp interior
-# pp boundary_len
-
-# area = interior + boundary_len / 2 - 1
-# puts area
