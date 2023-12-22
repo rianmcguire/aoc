7f31303607b50ffff6fa58f0fcd44129628c87e0
Rian McGuire <rian@rian.id.au>
2023-12-23 08:53:07 +1100

    Hide .exe in perf

--- perf.rb
+++ perf.rb
@@ -5,12 +5,16 @@ require 'open3'
 
 prefix_filter = ARGV[0]
 
+def label(file)
+  file.gsub(".exe", "")
+end
+
 files = Dir["*.rb", "*.exe"].sort.reject { File.identical?(_1, __FILE__) || _1.include?("bench") }.filter { prefix_filter.nil? || _1.start_with?(prefix_filter) }
-max_length = files.map(&:length).max
+max_length = files.map { label(_1).length }.max
 
 files.each do |file|
   input = "#{file.split(".").first.gsub("b", "")}.txt"
-  STDOUT.write file.ljust(max_length, " ")
+  STDOUT.write label(file).ljust(max_length, " ")
   STDOUT.write " -"
   3.times do
     t = Benchmark.realtime do
