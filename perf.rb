baf3b9b424751858be74619b3aa5a51136fc6bfb
Rian McGuire <rian@rian.id.au>
2023-12-24 10:38:07 +1100

    perf memory usage

--- perf.rb
+++ perf.rb
@@ -15,14 +15,16 @@ max_length = files.map { label(_1).length }.max
 files.each do |file|
   input = "#{file.split(".").first.gsub("b", "")}.txt"
   STDOUT.write label(file).ljust(max_length, " ")
-  STDOUT.write " -"
-  3.times do
-    t = Benchmark.realtime do
-      stdout, stderr, status = Open3.capture3("./#{file}", "#{input}")
-      raise "Failed: #{stdout} #{stderr} #{status.inspect}" if status.exitstatus != 0
-    end
-    STDOUT.write " "
-    STDOUT.write sprintf("%2.3fs", t)
-  end
-  puts
+  STDOUT.write " "
+    t, max_rss = 3.times.map do
+    stdout, stderr, status = Open3.capture3("/usr/bin/time", "-l", "./#{file}", "#{input}")
+    raise "Failed: #{stdout} #{stderr} #{status.inspect}" if status.exitstatus != 0
+    t = stderr.match(/([\d.]+) real/)[1].to_f
+    max_rss = stderr.match(/(\d+).*maximum resident set size/)[1].to_f
+    STDOUT.write "-"
+
+    [t, max_rss]
+  end.min_by { |t, max_rss| t }
+  max_rss_mb = max_rss / 1024 / 1024
+  STDOUT.puts sprintf(" %6.3fs %5.1fMB", t, max_rss_mb)
 end
