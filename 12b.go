6055e2f8363fe11d9ee456f41864ba461ca816ee
Rian McGuire <rian@rian.id.au>
2023-12-31 16:25:13 +1100

    Go with bitmasks

--- 12b.go
+++ 12b.go
@@ -35,31 +35,42 @@ func main() {
 			}
 			counts = append(counts, count)
 		}
-		sum += search([]rune(springs), counts, make(map[string]int64))
+		sum += search([]rune(springs), counts, 0, 0, make(map[memoKey]int64))
 	}
 
 	fmt.Println(sum)
 }
 
-func search(springs []rune, counts []int, memo map[string](int64)) int64 {
-	for len(springs) > 0 && springs[0] == '.' {
+type memoKey struct {
+	springs_len  int
+	counts_len   int
+	working_mask uint16
+	broken_mask  uint16
+}
+
+func search(springs []rune, counts []int, working_mask uint16, broken_mask uint16, memo map[memoKey](int64)) int64 {
+	for len(springs) > 0 && springs[0] == '.' || working_mask&1 == 1 {
 		springs = springs[1:]
+		working_mask >>= 1
+		broken_mask >>= 1
 	}
 
-	memo_key := fmt.Sprintf("%s%d", string(springs), len(counts))
+	memo_key := memoKey{springs_len: len(springs), counts_len: len(counts), working_mask: working_mask, broken_mask: broken_mask}
 	result, ok := memo[memo_key]
 	if ok {
 		return result
 	}
 
 	leading_springs := 0
+	var search_mask uint16 = 1
 	for i, s := range springs {
-		if s != '#' {
+		if s != '#' && search_mask&broken_mask == 0 {
 			break
 		}
 		leading_springs = i + 1
+		search_mask <<= 1
 	}
-	complete_group := leading_springs > 0 && (leading_springs > len(springs)-1 || springs[leading_springs] == '.')
+	complete_group := leading_springs > 0 && (leading_springs > len(springs)-1 || springs[leading_springs] == '.' || working_mask&(1<<leading_springs) != 0)
 
 	if len(springs) == 0 && len(counts) == 0 {
 		result = 1
@@ -69,28 +80,21 @@ func search(springs []rune, counts []int, memo map[string](int64)) int64 {
 		result = 0
 	} else if complete_group {
 		if leading_springs == counts[0] {
-			result = search(springs[counts[0]:], counts[1:], memo)
+			result = search(springs[counts[0]:], counts[1:], working_mask>>counts[0], broken_mask>>counts[0], memo)
 		} else {
 			result = 0
 		}
 	} else {
-		var unknown_idx int
-		for i, s := range springs {
-			if s == '?' {
-				unknown_idx = i
+		search_mask = 1
+		for _, s := range springs {
+			if s == '?' && (search_mask&(working_mask|broken_mask) == 0) {
 				break
 			}
+			search_mask <<= 1
 		}
 
-		before := springs[unknown_idx]
-
-		springs[unknown_idx] = '.'
-		with_working := search(springs, counts, memo)
-
-		springs[unknown_idx] = '#'
-		with_broken := search(springs, counts, memo)
-
-		springs[unknown_idx] = before
+		with_working := search(springs, counts, working_mask|search_mask, broken_mask, memo)
+		with_broken := search(springs, counts, working_mask, broken_mask|search_mask, memo)
 
 		result = with_working + with_broken
 	}
