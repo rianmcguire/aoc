81ab9cbd2652c891dc5dde9d1e3c03c4054c48e1
Rian McGuire <rian.mcguire@buildkite.com>
2024-12-02 16:19:40 +1100

    Day 2 2024 tidy

--- 2024/02.rb
+++ 2024/02.rb
@@ -1,12 +1,17 @@
 #!/usr/bin/env ruby
 
+def safe?(levels)
+  all_inc = levels.sort == levels
+  all_dec = levels.sort.reverse == levels
+  diffs = levels.drop(1).zip(levels)
+    .map { |a,b| (a-b).abs }
+    .all? { |d| (1..3).include?(d) }
+
+  (all_inc || all_dec) && diffs
+end
+
 ARGF.each_line.filter do |line|
   levels = line.split.map(&:to_i)
 
-  all_inc = levels.sort == levels
-  all_dec = levels.sort.reverse == levels
-
-  diffs = levels.drop(1).zip(levels).map { |a,b| (a-b).abs }
-
-  (all_inc || all_dec) && (diffs.all? { |d| d >= 1 && d <= 3 })
+  safe?(levels)
 end.count.then { puts _1 }
--- 2024/02b.rb
+++ 2024/02b.rb
@@ -3,29 +3,24 @@
 def safe?(levels)
   all_inc = levels.sort == levels
   all_dec = levels.sort.reverse == levels
+  diffs = levels.drop(1).zip(levels)
+    .map { |a,b| (a-b).abs }
+    .all? { |d| (1..3).include?(d) }
 
-  diffs = levels.drop(1).zip(levels).map { |a,b| (a-b).abs }
+  (all_inc || all_dec) && diffs
+end
 
-  (all_inc || all_dec) && (diffs.all? { |d| d >= 1 && d <= 3 })
+def try_removals(levels)
+  (0...levels.length).each do |i|
+    modified = levels.dup
+    modified.delete_at(i)
+    return true if safe?(modified)
+  end
+  false
 end
 
 ARGF.each_line.filter do |line|
   levels = line.split.map(&:to_i)
 
-  if !safe?(levels)
-    found_safe = false
-    (0..levels.length).each do |i|
-      modified = levels.dup
-      modified.delete_at(i)
-
-      if safe?(modified)
-        found_safe = true
-        break
-      end
-    end
-
-    found_safe
-  else
-    true
-  end
+  safe?(levels) || try_removals(levels)
 end.count.then { puts _1 }
