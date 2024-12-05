e06b463fbae0b978d91d8a339f41a0bdcd4f4692
Rian McGuire <rian.mcguire@buildkite.com>
2024-12-05 16:28:36 +1100

    Day 4 2024 tidy

--- 2024/04.rb
+++ 2024/04.rb
@@ -1,25 +1,27 @@
 #!/usr/bin/env ruby
 
+DIRS = %i(n s e w ne nw se sw)
+
 Pos = Struct.new(:x, :y) do
   def step(dir)
-      case dir
-      when "n"
-          Pos.new(x, y - 1)
-      when "s"
-          Pos.new(x, y + 1)
-      when "e"
-          Pos.new(x + 1, y)
-      when "w"
-          Pos.new(x - 1, y)
-      when "ne"
-          Pos.new(x + 1, y - 1)
-      when "nw"
-          Pos.new(x - 1, y - 1)
-      when "se"
-          Pos.new(x + 1, y + 1)
-      when "sw"
-          Pos.new(x - 1, y + 1)
-      end
+    case dir
+    when :n
+      Pos.new(x, y - 1)
+    when :s
+      Pos.new(x, y + 1)
+    when :e
+      Pos.new(x + 1, y)
+    when :w
+      Pos.new(x - 1, y)
+    when :ne
+      Pos.new(x + 1, y - 1)
+    when :nw
+      Pos.new(x - 1, y - 1)
+    when :se
+      Pos.new(x + 1, y + 1)
+    when :sw
+      Pos.new(x - 1, y + 1)
+    end
   end
 
   def valid?
@@ -27,36 +29,34 @@ Pos = Struct.new(:x, :y) do
   end
 end
 
-grid = ARGF.each_line.map do |row|
+GRID = ARGF.each_line.map do |row|
   row.chomp.chars
 end
 
-Y_RANGE = 0...grid.length
-X_RANGE = 0...grid.first.length
-
-DIRS = %w(n s e w ne nw se sw)
+Y_RANGE = 0...GRID.length
+X_RANGE = 0...GRID.first.length
 
 TARGET = "XMAS"
 
+def match?(pos, dir)
+  string = ""
+  while pos.valid?
+    string += GRID[pos.y][pos.x]
+    return false unless TARGET.start_with?(string)
+    return true if string == TARGET
+    pos = pos.step(dir)
+  end
+
+  false
+end
+
 result = 0
-grid.each_with_index do |row, y|
+GRID.each_with_index do |row, y|
   row.each_with_index do |cell, x|
     next unless cell == "X"
 
-    start = Pos.new(x, y)
-
     DIRS.each do |dir|
-      pos = start
-      string = ""
-      while pos.valid?
-        string += grid[pos.y][pos.x]
-        break if string == TARGET || !TARGET.start_with?(string)
-        pos = pos.step(dir)
-      end
-
-      if string == TARGET
-        result += 1
-      end
+      result += 1 if match?(Pos.new(x, y), dir)
     end
   end
 end
--- 2024/04b.rb
+++ 2024/04b.rb
@@ -2,24 +2,24 @@
 
 Pos = Struct.new(:x, :y) do
   def step(dir)
-      case dir
-      when "n"
-          Pos.new(x, y - 1)
-      when "s"
-          Pos.new(x, y + 1)
-      when "e"
-          Pos.new(x + 1, y)
-      when "w"
-          Pos.new(x - 1, y)
-      when "ne"
-          Pos.new(x + 1, y - 1)
-      when "nw"
-          Pos.new(x - 1, y - 1)
-      when "se"
-          Pos.new(x + 1, y + 1)
-      when "sw"
-          Pos.new(x - 1, y + 1)
-      end
+    case dir
+    when :n
+      Pos.new(x, y - 1)
+    when :s
+      Pos.new(x, y + 1)
+    when :e
+      Pos.new(x + 1, y)
+    when :w
+      Pos.new(x - 1, y)
+    when :ne
+      Pos.new(x + 1, y - 1)
+    when :nw
+      Pos.new(x - 1, y - 1)
+    when :se
+      Pos.new(x + 1, y + 1)
+    when :sw
+      Pos.new(x - 1, y + 1)
+    end
   end
 
   def valid?
@@ -27,29 +27,29 @@ Pos = Struct.new(:x, :y) do
   end
 end
 
-grid = ARGF.each_line.map do |row|
+GRID = ARGF.each_line.map do |row|
   row.chomp.chars
 end
 
-Y_RANGE = 0...grid.length
-X_RANGE = 0...grid.first.length
+Y_RANGE = 0...GRID.length
+X_RANGE = 0...GRID.first.length
+
+def match?(pos)
+  diags = [[pos.step(:sw), pos.step(:ne)], [pos.step(:se), pos.step(:nw)]]
+
+  match = diags.all? do |diag|
+    next unless diag.all? { _1.valid? }
+    s = diag.map { GRID[_1.y][_1.x] }.join
+    s == "SM" || s == "MS"
+  end
+end
 
 result = 0
-grid.each_with_index do |row, y|
+GRID.each_with_index do |row, y|
   row.each_with_index do |cell, x|
     next unless cell == "A"
 
-    start = Pos.new(x, y)
-
-    diags = [[start.step("sw"), start.step("ne")], [start.step("se"), start.step("nw")]]
-
-    match = diags.all? do |diag|
-      next unless diag.all? { _1.valid? }
-      s = diag.map { grid[_1.y][_1.x] }.join
-      s == "SM" || s == "MS"
-    end
-
-    result += 1 if match
+    result += 1 if match?(Pos.new(x, y))
   end
 end
 
