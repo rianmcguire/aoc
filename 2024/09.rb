e24d778c68f536ad1dc49e0f4dca47533c6b3869
Rian McGuire <rian@rian.id.au>
2024-12-09 18:00:47 +1100

    Day 9 2024 optimize part 1

--- 2024/09.rb
+++ 2024/09.rb
@@ -1,14 +1,14 @@
 #!/usr/bin/env ruby
 
-FileRecord = Struct.new(:id, :length) do
-  def to_s
-    id.to_s * length
-  end
-end
+RubyVM::YJIT.enable
 
-SpaceRecord = Struct.new(:length) do
-  def to_s
-    "." * length
+Record = Struct.new(:id, :length) do
+  def file?
+    id
+  end
+
+  def space?
+    !id
   end
 end
 
@@ -19,41 +19,59 @@ space = false
 ARGF.read.chomp.each_char do |c|
   c = c.to_i
   if space
-    disk << SpaceRecord.new(c) if c > 0
+    disk << Record.new(nil, c) if c > 0
   else
-    disk << FileRecord.new(id, c)
+    disk << Record.new(id, c)
     id += 1
   end
 
   space = !space
 end
 
+file_index = space_index = nil
 loop do
-  # puts disk.map { |r| r.to_s }.join
-
-  while disk.last.is_a?(SpaceRecord)
+  # Prune any trailing space so we don't have to search through it
+  while disk.last.space?
     disk.pop
   end
 
-  space_index = disk.index { _1.is_a?(SpaceRecord) }
-  break if space_index.nil?
+  # Find the last file
+  file_index ||= disk.rindex { _1.file? }
+
+  # Find the first free space
+  space_index ||= disk.index { _1.space? }
+
+  break if space_index > file_index
+
   space = disk[space_index]
+  file = disk[file_index]
 
-  f = disk.last
-  f.length -= 1
-  disk.delete(f) if f.length == 0
-
-  disk.insert(space_index, FileRecord.new(f.id, 1))
-  space_index += 1
+  # Move one block of the file into the space
+  file.length -= 1
+  disk.insert(space_index, Record.new(file.id, 1))
   space.length -= 1
-  disk.delete_at(space_index) if space.length == 0
+
+  # We've inserted - adjust the indexes
+  space_index += 1
+  file_index += 1
+
+  if file.length == 0
+    disk.delete_at(file_index)
+    file_index = nil
+  end
+
+  if space.length == 0
+    disk.delete_at(space_index)
+    space_index = nil
+    file_index -= 1 if file_index
+  end
 end
 
 result = 0
 i = 0
 disk.each do |r|
   r.length.times do
-    result += i * r.id
+    result += i * r.id if r.file?
     i += 1
   end
 end
