20ccd6621de04d6872406b4b522c0d9373250cac
Rian McGuire <rian@rian.id.au>
2024-12-27 17:11:40 +1100

    Day 24 2024 part 2

--- 2024/24b.rb
+++ 2024/24b.rb
@@ -1,119 +1,112 @@
 #!/usr/bin/env ruby
 
-require 'tsort'
+Gate = Struct.new(:op, :a, :b, :name) do
+  def a
+    return self[:a] if input?(self[:a])
+    GATES[self[:a]]
+  end
 
-initial, gates = ARGF.read.split("\n\n")
+  def b
+    return self[:b] if input?(self[:b])
+    GATES[self[:b]]
+  end
 
-class Deps < Hash
-  include TSort
+  def operands
+    [a, b]
+  end
 
-  alias tsort_each_node each_key
-  def tsort_each_child(node, &block)
-    fetch(node).each(&block)
+  def <=>(other)
+    sort_key <=> other.sort_key
+  end
+
+  def sort_key
+    [op, self[:a], self[:b]]
+  end
+
+  def input?(gate)
+    gate[0] == "x" || gate[0] == "y"
   end
 end
 
-GATE_VALUES = {}
+initial, gates = ARGF.read.split("\n\n")
 
-DEPS = Deps.new
-initial.each_line(chomp: true).each do |l|
-  gate, value = l.split(": ")
-  value = value.to_i
-
-  DEPS[gate] = []
-  GATE_VALUES[gate] = value
-end
+GATES = {}
 
 gates.each_line(chomp: true).each do |l|
   a, op, b, _, out = l.split(" ")
 
-  DEPS[out] = [a,b]
-
-  GATE_VALUES[out] = [op, a, b]
+  GATES[out] = Gate.new(op, a, b, out)
 end
 
-OPS = {
-  "XOR" => :^,
-  "OR" => :|,
-  "AND" => :&,
-}
+output_gate_names = GATES.keys.filter { _1[0] == "z" }.sort
 
-def run(gate_values, deps)
-  gate_values = gate_values.dup
+bad_gates = []
+output_gate_names
+  .each_with_index do |gate, i|
+    n = sprintf('%02d', i)
+    n_prev = sprintf('%02d', i-1)
 
-  deps.tsort.each do |gate|
-    next if gate_values[gate].is_a?(Integer)
+    # Special-ish cases. These are correct in my input data, so don't bother handling them
+    next if ["z00", "z01", "z02", "z45"].include?(gate)
 
-    op, a, b = gate_values[gate]
-    op = OPS[op]
-    a = gate_values[a]
-    b = gate_values[b]
+    g = GATES[gate]
 
-    gate_values[gate] = a.send(op, b)
+    # Validate that the definition of this output looks like an adder with this shape:
+    # z[n] = (xor
+    #   (xor x[n] y[n])
+    #   carry[n-1]
+    # )
+    # carry[n] = (or
+    #   (and x[n] y[n])
+    #   (and c_out[n-1] (xor x[n] y[n]))
+    # )
+
+    if g.op != "XOR"
+      puts "#{gate}: no top-level XOR"
+      bad_gates << gate
+      next
+    end
+
+    input_xor = g.operands.find { |ex| ex.op == "XOR" && ex.operands.sort == ["x#{n}", "y#{n}"] }
+    if !input_xor
+      puts "#{gate}: no input XOR"
+      # TODO: hard coded assumption based on input
+      bad_gates << g.operands.first.name
+      next
+    end
+
+    carry = g.operands.find { _1 != input_xor }
+    if carry.op != "OR"
+      puts "#{gate}: unexpected carry operation"
+      bad_gates << carry.name
+      next
+    end
+
+    c1 = carry.operands.find do |o|
+      o.op == "AND" && o.operands.sort == ["x#{n_prev}", "y#{n_prev}"]
+    end
+    if !c1
+      puts "#{gate}: could not find (and x[n] y[n])"
+      # TODO: hard coded assumption based on input
+      bad_gates << carry.operands.last.name
+      next
+    end
+
+    c2 = carry.operands.find { _1 != c1 && _1.op == "AND" }
+    if !c2
+      puts "#{gate}: could not find (and c_out[n-1] (xor x[n] y[n]))"
+      # TODO: hard coded assumption based on input
+      bad_gates << carry.operands.last.name
+      next
+    end
+    c2_xor = c2.operands.find { |ex| ex.op == "XOR" }
+
+    prev_prev_carry = c2.operands.find { _1 != c2_xor }
+    if prev_prev_carry.op != "OR"
+      puts "#{gate}: unexpected carry in"
+      bad_gates << prev_prev_carry.name
+      next
+    end
   end
 
-  gate_values
-end
-
-def result(prefix, gate_values)
-  gate_values.keys
-    .filter { _1[0] == prefix }
-    .sort.reverse
-    .reduce(0) { |memo, gate| (memo << 1) | gate_values[gate] }
-end
-
-def set_gates(prefix, gate_values, n)
-  n_gates = gate_values.keys.count { _1[0] == prefix }
-
-  result = []
-  mask = 1
-  n_gates.times do |i|
-    result << "#{prefix}#{sprintf('%02d', i)}" if n.anybits?(mask)
-    mask <<= 1
-  end
-  result
-end
-
-gate_values = run(GATE_VALUES, DEPS)
-x = result("x", gate_values)
-y = result("y", gate_values)
-expected = x & y
-actual = result("z", gate_values)
-
-diff = expected ^ actual
-bad_gates = set_gates("z", gate_values, diff)
-
-def upstreams(gate)
-  [gate, *DEPS[gate].flat_map { upstreams(_1) }]
-end
-
-def swap(hash, a, b)
-  temp = hash[a]
-  hash[a] = hash[b]
-  hash[b] = temp
-  nil
-end
-
-all_in_path = bad_gates.flat_map { |g| upstreams(g) }
-all_in_path.reject! { |g| g[0] == "x" || g[0] == "y" }
-pairs = all_in_path.combination(2).to_a
-pp pairs
-pairs.combination(2).each do |pairs|
-  gate_values = GATE_VALUES.dup
-  deps = DEPS.dup
-
-  # Try swapping all of the pairs
-  pairs.each do |a, b|
-    swap(gate_values, a, b)
-    swap(deps, a, b)
-  end
-
-  pp deps
-
-  gate_values = run(gate_values, deps)
-  if result("z", gate_values) == expected
-    pp "got it"
-    pp pairs
-    break
-  end
-end
+puts bad_gates.uniq.sort.join(",")
