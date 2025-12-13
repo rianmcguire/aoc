f05955a85b6254dd3d195d725dc2238494330af2
Rian McGuire <rian@rian.id.au>
2025-12-13 20:13:26 +1100

    Day 11 2025 part 1

---
+++ 2025/11.rb
@@ -0,0 +1,41 @@
+#!/usr/bin/env ruby
+
+graph = {}
+
+ARGF.each_line.map do |line|
+  from, to = line.chomp.split(": ")
+  to = to.split
+
+  graph[from] = to
+end
+
+def dfs(source:, adjacent_fn:, target_fn:)
+  stack = [[source, [source]]]
+  paths = []
+
+  while (node, path = stack.pop)
+    if target_fn.call(node)
+      paths << path
+    end
+
+    adjacent_fn.call(node).filter_map do |child|
+      next if path.include?(child)
+
+      new_path = path.dup
+      new_path << child
+      stack.push([child, new_path])
+    end
+  end
+
+  paths
+end
+
+puts dfs(
+  source: "you",
+  adjacent_fn: proc do |state|
+    graph.fetch(state, [])
+  end,
+  target_fn: proc do |state|
+    state == "out"
+  end,
+).length
