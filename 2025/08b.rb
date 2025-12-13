2d5e2dbe1d37c5c4009f62991a73ec7725bd7918
Rian McGuire <rian@rian.id.au>
2025-12-13 13:30:29 +1100

    Day 8 2025

---
+++ 2025/08b.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/env ruby
+
+Pos = Struct.new(:x, :y, :z) do
+  def dist(b)
+    a = self
+    Math.sqrt((a.x - b.x).pow(2) + (a.y - b.y).pow(2) + (a.z - b.z).pow(2))
+  end
+end
+
+boxes = ARGF.each_line.map do |line|
+  Pos.new(*line.split(",").map(&:to_i))
+end
+
+circuit_by_box = {}
+boxes_by_circuit = Hash.new { |h,k| h[k] = Set.new }
+
+boxes.each_with_index do |box, circuit|
+  circuit_by_box[box] = circuit
+  boxes_by_circuit[circuit] << box
+end
+
+boxes.combination(2).sort_by { |a, b| a.dist(b) }.each do |a, b|
+  next if circuit_by_box[a] == circuit_by_box[b]
+
+  # Merge circuits. Arbitrarily choose the first one
+  circuit = circuit_by_box[a]
+  old_circuit = circuit_by_box[b]
+  to_update = boxes_by_circuit.delete(old_circuit)
+  to_update.each do |box|
+    circuit_by_box[box] = circuit
+    boxes_by_circuit[circuit] << box
+  end
+
+  if boxes_by_circuit.length == 1
+    puts a.x * b.x
+    break
+  end
+end
